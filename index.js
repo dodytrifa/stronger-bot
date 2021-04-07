4284b8b7c4d8a437b320d2248eb0f0d4255c88f5
dodytrifa <dody.trifa@gmail.com>
2021-04-07 21:45:32 +0700

    ready to activate with uptime robot

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+require('dotenv').config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
+const fetch = require('node-fetch')
 
 client.on("ready", () => {
-  console.log('logged in')
+  console.log('User logged in')
 })
 
 client.on("message", msg => {
-  if(msg.content === "ping"){
-    msg.reply("pong")
+  if(msg.author.bot){
+    return
+  }
+
+  if(msg.content === "$start"){
+    getResponse().then(quote => msg.channel.send(quote))
   }
 })
 
-client.login()
+client.login(process.env.TOKEN)
+
+function getResponse() {
+  return fetch('https://zenquotes.io/api/random')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]['q'] + ' - ' + data[0]['a']
+    })
+}
